1e9ca50d68b399741c236a0e095646c0eebd5e40
framoralc <framoralc@alu.edu.gva.es>
2025-11-06 16:26:04 +0100

    Test corregidos

---
+++ src/fibonacci.js
@@ -0,0 +1,7 @@
+function fibonacci() {
+
+}
+
+function esPinValido(){
+    
+}
--- test/fibo.test.js
+++ test/fibo.test.js
@@ -1,34 +1,34 @@
-import {describe, it, except} from 'vitest'
+import {describe, it, expect} from 'vitest'
 import {fibonacci, esPinValido} from '../src/fibonacci.js'
 
 describe('fibonacci', () => {
     it('fibonacci n = 0', () => {
-        except(fibonacci(0).toBe(0))
+        expect(fibonacci(0).toBe(0))
     })
 
     it('fibonacci n = 1', () => {
-        except(fibonacci(1).toBe(1))
+        expect(fibonacci(1).toBe(1))
     })
 
     it('fibonacci n = 5', () => {
-        except(fibonacci(5).toBe(3))
+        expect(fibonacci(5).toBe(3))
     })
 })
 
 describe('esPinValido', () => {
     it('null no es pin Valido', () =>{
-        except(esPinValido().toBe(false))
+        expect(esPinValido().toBe(false))
     })
 
     it('Longitud de 4 o 6', () => {
-        except(esPinValido(12345).toBe(true))
+        expect(esPinValido('12345').toBe(true))
     })
 
     it('Solo digitos 0-9', () => {
-        except(esPinValido(e3e3r).toBe(false))
+        expect(esPinValido('e3e3r').toBe(false))
     })
 
     it('No pueden ser iguales los dígitos' , () => {
-        except(esPinValido(11111).toBe(false))
+        expect(esPinValido('11111').toBe(false))
     })
 })
